5104e43d0a2cffcb47f7d8742a7d4725f0051bb6
Martin Lundfall <martin.lundfall@gmail.com>
2022-12-09 18:21:15 +0100

    with rnbo patch

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const path = require('path');
-//const five = require("johnny-five");
 const osc = require('osc');
 //const board = new five.Board();
 
 // For serving static assets
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.use('/css', express.static(__dirname + '/css'));
 
@@ -27,37 +26,6 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log("listening on *:3000");
 })
-// board.on("ready", function() {
-//     console.log("board online");
-//   var gyro = new five.Gyro({
-//       controller: "MPU6050",
-//   });
-//   gyro.recalibrate();// Tell the device to recalibrate
-//     var pitch = gyro.pitch.angle;
-//     var roll  = gyro.roll.angle;
-//     var yaw   = gyro.yaw.angle;
-//     gyro.on("change", function() {
-//       if (   Math.abs(this.pitch.angle - pitch) > 1
-// 	  || Math.abs(this.roll.angle - roll) > 1
-// 	  || Math.abs(this.yaw.angle - yaw) > 1
-// 	 ) {
-// 	  console.log("gyro");
-// 	  console.log("  x            : ", this.x);
-// 	  console.log("  y            : ", this.y);
-// 	  console.log("  z            : ", this.z);
-// 	  console.log("  pitch        : ", this.pitch);
-// 	  console.log("  roll         : ", this.roll);
-// 	  console.log("  yaw          : ", this.yaw);
-// 	  console.log("  rate         : ", this.rate);
-// 	  pitch = gyro.pitch.angle;
-// 	  roll  = gyro.roll.angle;
-// 	  yaw   = gyro.yaw.angle;
-// 	  io.emit('gyro', {pitch: this.pitch, roll: this.roll, yaw: this.yaw})
-//       }
-//   });
-// })
-// Create an osc.js UDP Port listening on port 57121.
-
 var udpPort = new osc.UDPPort({
     localAddress: "0.0.0.0",
     //    localAddress: "127.0.0.1",
@@ -65,6 +33,10 @@ var udpPort = new osc.UDPPort({
     metadata: true
 });
 
+// Open the socket.
+udpPort.open();
+
+// arduino -> websocket connection
 // Listen for incoming OSC messages.
 let i = 0;
 let pitch;
@@ -99,6 +71,3 @@ udpPort.on("message", function (oscMsg, timeTag, info) {
 	io.emit('gyro', {pitch: accAngleX, roll: accAngleY, yaw: yaw});
     }
 });
-
-// Open the socket.
-udpPort.open();
